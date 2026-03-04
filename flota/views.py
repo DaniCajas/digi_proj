959452952a7462fe3c6ea1a3ff807c6b3b86c2a0
matias0502 <maro014@vidalibarraquer.net>
2026-03-04 20:10:47 +0100

    Add files via upload

--- flota/views.py
+++ flota/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def inicio(request):
+    return HttpResponse("<h1>¡Bienvenido a Dakkar Car Rental!</h1> <p>Esta será la página principal de nuestra flota.</p>")
 # Create your views here.
